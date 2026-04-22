8801ed84a79ccd5419a98bf60cb8d0bde8222669
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-04-22 14:56:11 +0530

    Add packageRoute

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,10 +1,16 @@
 import { Hono } from 'hono';
 
 import homeRoute from '@/routes/home';
+import packageRoute from '@/routes/package';
 
 const app = new Hono();
 
+app.notFound((c) => {
+  return c.text('Not Found', 404);
+});
+
 app.route('/', homeRoute);
+app.route('/package', packageRoute);
 
 export default {
   fetch: app.fetch,
