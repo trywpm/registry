28fc690b81526cd38f36f6850884fad39de723af
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-04-26 00:25:06 +0530

    Add plugins and themes routes

--- web/src/routes/home.ts
+++ web/src/routes/home.ts
@@ -1,10 +1,21 @@
 import { Hono } from 'hono';
+
 import { HomePage } from '@/pages/home';
+import { ThemesPage } from '@/pages/themes';
+import { PluginsPage } from '@/pages/plugins';
 
 const homeRoute = new Hono();
 
 homeRoute.get('/', (c) => {
-  return c.html(HomePage(c));
+  return HomePage(c);
+});
+
+homeRoute.get('/plugins', (c) => {
+  return PluginsPage(c);
+});
+
+homeRoute.get('/themes', (c) => {
+  return ThemesPage(c);
 });
 
 export default homeRoute;
