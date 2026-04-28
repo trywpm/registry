710e4fa55f7d458ff945b2db4bbcd7c040d5743f
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-04-28 16:00:33 +0530

    Remove min-h-screen from page level

--- web/src/pages/500.tsx
+++ web/src/pages/500.tsx
@@ -12,10 +12,12 @@ export function ServerError(c: Context, err: Error) {
       title="Internal Server Error"
       description="A server error occurred while processing your request."
     >
-      <main class="flex min-h-screen w-full items-center justify-center p-6 md:p-10">
-        <div class="flex flex-col items-center gap-4 text-center">
-          <p class="font-mono">ERROR 500</p>
-          <h1 class="text-3xl sm:text-5xl">Oops! Something went wrong.</h1>
+      <main class="flex grow items-center justify-center p-6 md:p-10">
+        <div class="container">
+          <div class="flex flex-col items-center gap-4 text-center">
+            <p class="font-mono">ERROR 500</p>
+            <h1 class="text-3xl sm:text-5xl">Oops! Something went wrong.</h1>
+          </div>
         </div>
       </main>
     </BaseLayout>,
