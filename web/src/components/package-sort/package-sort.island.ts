a649dfaa0f6ecb855e7aba4d39b237b21226528d
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-04-28 13:50:21 +0530

    Replace click event with `wpm-select-action` event

--- web/src/components/package-sort/package-sort.island.ts
+++ web/src/components/package-sort/package-sort.island.ts
@@ -26,19 +26,19 @@ class PackageSort extends HTMLElement {
     const { signal } = this.abortController;
 
     this.addEventListener(
-      'click',
+      'wpm-select-action',
       (e: Event) => {
-        if (!(e.target instanceof HTMLElement)) {
+        if (!(e instanceof CustomEvent) || !e.detail || e.detail.item === undefined) {
           return;
         }
 
-        const sortItem = e.target.closest<HTMLElement>('[data-sort-value]');
+        if (!(e.detail.item instanceof HTMLElement)) {
+          return;
+        }
 
-        if (sortItem) {
-          const sortKey = sortItem.getAttribute('data-sort-value');
-          if (sortKey) {
-            this.handleSortChange(sortKey);
-          }
+        const sortVal = e.detail.item.getAttribute('data-sort-value');
+        if (sortVal) {
+          this.handleSortChange(sortVal);
         }
       },
       { signal },
