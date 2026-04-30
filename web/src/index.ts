297938305925465d7e0e12da90d3a207148482fe
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-04-30 23:55:41 +0530

    Add csp middleware

--- web/src/index.ts
+++ web/src/index.ts
@@ -8,8 +8,32 @@ import packageRoute from '@/routes/package';
 
 const app = new Hono<{
   Bindings: Cloudflare.Env;
-  Variables: {};
+  Variables: {
+    cspNonce: string;
+  };
 }>();
+
+app.use('*', async (c, next) => {
+  const nonce = btoa(crypto.randomUUID());
+  const csp = [
+    `base-uri 'self'`,
+    `default-src 'none'`,
+    `connect-src 'self'`,
+    `object-src 'none'`,
+    `form-action 'self'`,
+    `font-src 'self' https:`,
+    `frame-ancestors 'none'`,
+    `img-src 'self' data: https:`,
+    `style-src 'self' 'unsafe-inline'`,
+    `script-src 'nonce-${nonce}' 'self' 'unsafe-inline'`,
+    `frame-src 'self' https://www.youtube-nocookie.com https://videopress.com`,
+  ].join('; ');
+
+  c.set('cspNonce', nonce);
+  c.header('Content-Security-Policy', csp);
+
+  await next();
+});
 
 app.notFound((c) => {
   return NotFound(c);
