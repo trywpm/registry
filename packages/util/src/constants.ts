9ed4c9870086138d505ac8f26fcc93a15f9d1909
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-05-11 00:43:34 +0530

    Add user status constant

--- packages/util/src/constants.ts
+++ packages/util/src/constants.ts
@@ -7,7 +7,14 @@ export const PACKAGE_NAME_REGEX = NAMING_REGEX;
 
 // package types and visibility
 export const PACKAGE_TYPE = ['plugin', 'theme', 'mu-plugin'] as const;
+export const PACKAGE_STATUS = ['active', 'deprecated', 'deleted'] as const;
 export const PACKAGE_VISIBILITY = ['public', 'private', 'restricted'] as const;
 
 export type PackageType = (typeof PACKAGE_TYPE)[number];
+export type PackageStatus = (typeof PACKAGE_STATUS)[number];
 export type PackageVisibility = (typeof PACKAGE_VISIBILITY)[number];
+
+// user status
+export const USER_STATUS = ['active', 'inactive', 'banned', 'locked'] as const;
+
+export type UserStatus = (typeof USER_STATUS)[number];
