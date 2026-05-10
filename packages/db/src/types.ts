66a21d0ca652fc8863ce7178ca4b1256bb23bcd6
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-05-11 00:43:21 +0530

    Add types for db columns

---
+++ packages/db/src/types.ts
@@ -0,0 +1,3 @@
+export type UserId = string & { readonly __brand: 'UserId' };
+export type TokenId = string & { readonly __brand: 'TokenId' };
+export type PackageId = string & { readonly __brand: 'PackageId' };
