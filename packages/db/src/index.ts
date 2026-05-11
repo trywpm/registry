62e7b5afafdec9829def7f3c2a55f7592e874c42
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-05-11 20:08:57 +0530

    Add db queries registry

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,34 @@
-export * from './users';
-export * from './tokens';
-export * from './packages';
+import type { Sql } from 'postgres';
+
+import { Users } from './users';
+import { Tokens } from './tokens';
+import { Packages } from './packages';
+
+export class Registry {
+  private _users?: Users;
+  private _tokens?: Tokens;
+  private _packages?: Packages;
+
+  constructor(
+    private readonly db: () => Sql,
+    private readonly kv: KVNamespace,
+  ) {}
+
+  get users() {
+    this._users ??= new Users(this.db(), this.kv);
+    return this._users;
+  }
+
+  get packages() {
+    this._packages ??= new Packages(this.db(), this.kv);
+    return this._packages;
+  }
+
+  get tokens() {
+    this._tokens ??= new Tokens(this.db(), this.kv);
+    return this._tokens;
+  }
+}
+
+// export types
+export type { UserWithToken } from './users';
