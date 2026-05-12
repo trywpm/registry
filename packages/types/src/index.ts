520c46747df22b6f78688ccdd9f74be75cd91f04
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-05-12 22:44:18 +0530

    Add types package

---
+++ packages/types/src/index.ts
@@ -0,0 +1,3 @@
+export * from './enums';
+export type * from './ids';
+export * from './regexes';
---
+++ packages/types/src/regexes.ts
@@ -0,0 +1,5 @@
+const NAMING_REGEX = /^[a-z0-9]+(?:-[a-z0-9]+)*$/;
+
+export const DIST_TAG_REGEX = NAMING_REGEX;
+export const USERNAME_REGEX = NAMING_REGEX;
+export const PACKAGE_NAME_REGEX = NAMING_REGEX;
