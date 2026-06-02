f87794859273bfcb44e309ab857a1dcf25ffe684
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-06-02 15:37:28 +0530

    Add ErrorStatus type

--- packages/exception/src/user-error.ts
+++ packages/exception/src/user-error.ts
@@ -1,9 +1,11 @@
+type ErrorStatus = 400 | 401 | 403 | 404 | 413 | 422;
+
 export class UserError extends Error {
   override readonly name = 'UserError';
 
-  readonly status: number;
+  readonly status: ErrorStatus;
 
-  constructor(message: string, status = 400) {
+  constructor(message: string, status: ErrorStatus = 400) {
     super(message);
     this.status = status;
   }
