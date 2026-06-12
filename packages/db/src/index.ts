1ff7342bea12b8dbeafc804e8dea4ac813d6a00f
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-06-12 22:31:32 +0530

    Add pg instance creation

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,31 +1,42 @@
 import type { Sql } from 'postgres';
 
+import postgres from 'postgres';
+
 import { Users } from './users';
 import { Tokens } from './tokens';
 import { Packages } from './packages';
 
 export class Registry {
+  #sql: Sql | undefined;
+
   private _users?: Users;
   private _tokens?: Tokens;
   private _packages?: Packages;
 
   constructor(
-    private readonly db: () => Sql,
     private readonly kv: KVNamespace,
+    private readonly conStr: string,
   ) {}
+  private readonly db = (): Sql =>
+    (this.#sql ??= postgres(this.conStr, {
+      max: 1,
+      fetch_types: false,
+      idle_timeout: 15,
+      connect_timeout: 10,
+    }));
 
   get users() {
-    this._users ??= new Users(this.db(), this.kv);
+    this._users ??= new Users(this.db, this.kv);
     return this._users;
   }
 
   get packages() {
-    this._packages ??= new Packages(this.db(), this.kv);
+    this._packages ??= new Packages(this.db, this.kv);
     return this._packages;
   }
 
   get tokens() {
-    this._tokens ??= new Tokens(this.db(), this.kv);
+    this._tokens ??= new Tokens(this.db, this.kv);
     return this._tokens;
   }
 }
