615cdc1af5b75d228ba339b569a3726fd1b72bce
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-06-13 22:40:42 +0530

    Remove redundant queries

--- packages/db/src/tokens.ts
+++ packages/db/src/tokens.ts
@@ -1,5 +1,3 @@
-import type { TokenId } from '@wpm/types';
-
 import { Base } from './base';
 
 export class Tokens extends Base {
@@ -8,34 +6,6 @@ export class Tokens extends Base {
       UPDATE "token"
       SET "last_used" = now()
       WHERE "token_hash" = ${tokenHash}
-    `;
-  }
-
-  async deleteByHash(tokenHash: string) {
-    await this.invalidate(`user:by-token:${tokenHash}`);
-
-    await this.db`
-      DELETE FROM "token"
-      WHERE "token_hash" = ${tokenHash}
-    `;
-  }
-
-  async deleteById(tokenId: TokenId) {
-    const [row] = await this.db<[{ token_hash: string }?]>`
-      SELECT "token_hash"
-      FROM "token"
-      WHERE "id" = ${tokenId}
-    `;
-
-    if (!row) {
-      return;
-    }
-
-    await this.invalidate(`user:by-token:${row.token_hash}`);
-
-    await this.db`
-      DELETE FROM "token"
-      WHERE "id" = ${tokenId}
     `;
   }
 }
