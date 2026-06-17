0f6afa4a6809b842a9ff1ad99a8b9da4a6daa5db
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-06-17 21:47:00 +0530

    Replace node:net with isIP internal code

--- packages/net/src/net.ts
+++ packages/net/src/net.ts
@@ -1,4 +1,41 @@
-import { isIP } from 'node:net';
+// <node:net>
+// Source copied from <https://github.com/oven-sh/bun/blob/main/src/js/internal/net/isIP.ts>.
+const v4Seg = '(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])';
+const v4Str = `(?:${v4Seg}\\.){3}${v4Seg}`;
+let IPv4Reg: RegExp | undefined;
+
+const v6Seg = '(?:[0-9a-fA-F]{1,4})';
+let IPv6Reg: RegExp | undefined;
+
+export function isIPv4(s: string) {
+  return (IPv4Reg ??= new RegExp(`^${v4Str}$`)).test(s);
+}
+
+export function isIPv6(s: string) {
+  return (IPv6Reg ??= new RegExp(
+    '^(?:' +
+      `(?:${v6Seg}:){7}(?:${v6Seg}|:)|` +
+      `(?:${v6Seg}:){6}(?:${v4Str}|:${v6Seg}|:)|` +
+      `(?:${v6Seg}:){5}(?::${v4Str}|(?::${v6Seg}){1,2}|:)|` +
+      `(?:${v6Seg}:){4}(?:(?::${v6Seg}){0,1}:${v4Str}|(?::${v6Seg}){1,3}|:)|` +
+      `(?:${v6Seg}:){3}(?:(?::${v6Seg}){0,2}:${v4Str}|(?::${v6Seg}){1,4}|:)|` +
+      `(?:${v6Seg}:){2}(?:(?::${v6Seg}){0,3}:${v4Str}|(?::${v6Seg}){1,5}|:)|` +
+      `(?:${v6Seg}:){1}(?:(?::${v6Seg}){0,4}:${v4Str}|(?::${v6Seg}){1,6}|:)|` +
+      `(?::(?:(?::${v6Seg}){0,5}:${v4Str}|(?::${v6Seg}){1,7}|:))` +
+      ')(?:%[0-9a-zA-Z-.:]{1,})?$',
+  )).test(s);
+}
+
+export function isIP(s: string) {
+  if (isIPv4(s)) {
+    return 4;
+  }
+  if (isIPv6(s)) {
+    return 6;
+  }
+  return 0;
+}
+// </node:net>
 
 /**
  * Strips the IPv4-mapped IPv6 prefix (::ffff:) often attached by proxies.
