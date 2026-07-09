6b41287ee83d732a5c4c3ee616a270853290fb4b
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-07-09 19:59:40 +0530

    Remove private props

--- registry/src/lib/context.ts
+++ registry/src/lib/context.ts
@@ -8,39 +8,35 @@ import { authenticate } from '@/lib/auth';
 let baseLogger: Logger | undefined;
 
 export class RequestContext {
-  readonly req: Request;
-  readonly env: Cloudflare.Env;
-  readonly ctx: ExecutionContext;
+  declare private _user: Promise<UserWithToken | Response | null> | undefined;
+  declare private _repos: Registry | undefined;
+  declare private _logger: Logger | undefined;
+  declare private _requestId: string | undefined;
 
-  #user: Promise<UserWithToken | Response | null> | undefined;
-  #repos: Registry | undefined;
-  #logger: Logger | undefined;
-  #requestId: string | undefined;
-
-  constructor(req: Request, env: Cloudflare.Env, ctx: ExecutionContext) {
-    this.req = req;
-    this.env = env;
-    this.ctx = ctx;
-  }
+  constructor(
+    readonly req: Request,
+    readonly env: Cloudflare.Env,
+    readonly ctx: ExecutionContext,
+  ) {}
 
   waitUntil(promise: Promise<unknown>): void {
     this.ctx.waitUntil(promise);
   }
 
   get requestId(): string {
-    return (this.#requestId ??= this.req.headers.get('Cf-Ray') ?? crypto.randomUUID());
+    return (this._requestId ??= this.req.headers.get('Cf-Ray') ?? crypto.randomUUID());
   }
 
   get repos(): Registry {
-    return (this.#repos ??= new Registry(this.env.cache, this.env.pg.connectionString));
+    return (this._repos ??= new Registry(this.env.cache, this.env.pg.connectionString));
   }
 
   auth(): Promise<UserWithToken | Response | null> {
-    return (this.#user ??= authenticate(this));
+    return (this._user ??= authenticate(this));
   }
 
   logger(): Logger {
     baseLogger ??= new Logger(this.env.APP_ENV === 'development' ? 10 : 30, '');
-    return (this.#logger ??= baseLogger.child({ requestId: this.requestId }));
+    return (this._logger ??= baseLogger.child({ requestId: this.requestId }));
   }
 }
